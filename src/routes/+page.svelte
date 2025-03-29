7651daead316bfdd0a5ba0d07fbff3eabb5c6f02
Neville Brem <contact@nevillebrem.com>
2025-03-29 17:56:35 +0100

    just some styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,21 +5,33 @@
 	const chat = new Chat();
 </script>
 
-<main class="rounded-2xl border-2 border-gray-400 p-8">
-	<ul class="mb-4 h-[50dvh] overflow-y-auto rounded-lg border-2 border-gray-400 p-4">
+<main class="flex h-screen flex-col rounded-2xl p-4">
+	<h1
+		class="mb-2 bg-gradient-to-bl from-cyan-500 to-purple-500 bg-clip-text text-4xl font-black text-transparent italic"
+	>
+		NevGPT
+	</h1>
+	<ul class="mb-4 flex-grow space-y-4 overflow-y-auto rounded-lg border-2 border-gray-400 p-4">
 		{#each chat.messages as message}
-			<li class="flex flex-col gap-2">
-				<span>{message.role}:</span>
+			<li class="flex flex-col">
+				<span class="text-xl font-bold capitalize">{message.role}:</span>
 				<span class="prose">{@html marked(message.content)}</span>
 			</li>
 		{/each}
 	</ul>
 	<form onsubmit={chat.handleSubmit} class="flex gap-4">
-		<input class="flex-grow rounded-lg p-2 ring-2 ring-gray-400" bind:value={chat.input} />
+		<input
+			placeholder="What are you curious about?"
+			class="flex-grow rounded-lg p-2 ring-2 ring-gray-400"
+			bind:value={chat.input}
+		/>
 		{#if chat.status === 'streaming'}
-			<button class="rounded-lg bg-amber-300 px-4 py-2 font-bold" onclick={chat.stop}>Stop</button>
+			<button class="rounded-lg bg-rose-300 px-4 py-2 font-mono font-bold" onclick={chat.stop}
+				>Stop</button
+			>
 		{:else}
-			<button class="rounded-lg bg-sky-300 px-4 py-2 font-bold" type="submit">Send</button>
+			<button class="rounded-lg bg-sky-300 px-4 py-2 font-mono font-bold" type="submit">Send</button
+			>
 		{/if}
 	</form>
 </main>
